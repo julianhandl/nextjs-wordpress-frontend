f81c5ace4f7521182504fb7db4fd8d4ab86509af
Julian Handl <julian.handl@gmail.com>
2021-04-14 07:48:54 +0200

    update footer and header paths

--- components/DesignSystem/Header/Header.tsx
+++ components/DesignSystem/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { MenuNode } from "../../common/types/Menu";
+import { MenuNode } from "../../../common/types/Menu";
 
 export interface HeaderProps {
     menu?: MenuNode,
